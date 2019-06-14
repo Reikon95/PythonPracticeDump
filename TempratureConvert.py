8773f2978853a29d870454b032ed6d3ffc861ce2
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-06-14 14:45:16 +0100

    Create TempratureConvert.py

---
+++ TempratureConvert.py
@@ -0,0 +1,22 @@
+
+def f_to_c(f_temp):
+  c_temp = (f_temp - 32) * 5/9
+  print(c_temp)
+  
+  
+
+f_to_c(100)  
+
+
+def c_to_f(c_temp):
+  f_temp = (c_temp * (9/5)) + 32
+  print(f_temp)
+  
+  
+
+  
+c_to_f(0)  
+
+
+#first function is far to celsius, second is celsius to far
+
