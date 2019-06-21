016ab03cd653625e0c0e7ce481135ab6d45fe9bf
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-06-21 10:13:40 +0100

    Create RangesAndLists.py

---
+++ RangesAndLists.py
@@ -0,0 +1,6 @@
+list1 = range(5, 15, 3)
+#creates a range where you start from 5 (first param), end at 14 (one less than second param) and only count every third number (third param)
+
+
+print(list(list1))
+#prints it as a list, otherwise it'll just print range(5, 15, 3) literally
