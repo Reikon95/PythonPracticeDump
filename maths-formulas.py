c8248ece8b6dd589350401a9901a17a0dfe675b0
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-06-28 11:27:57 +0100

    Create maths-formulas.py

---
+++ maths-formulas.py
@@ -0,0 +1,7 @@
+
+#circle calculations
+radius = 5.
+circle_perimeter = (2 * 3.14 * radius) 
+circle_area = (3.14 * (radius ** 2)) 
+print('A circle of radius ' + str(radius) + ' has a perimeter of ' +
+   str(circle_perimeter) + ' and an area of ' + str(circle_area))
