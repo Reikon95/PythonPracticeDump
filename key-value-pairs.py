2fd6796f59560b7d8b7a0accedc911460072171b
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-12 15:31:30 +0100

    Update key-value-pairs.py

--- key-value-pairs.py
+++ key-value-pairs.py
@@ -18,3 +18,12 @@ animals_in_zoo["monkeys"] = 12
 animals_in_zoo["dinosaurs"] = 0
 print(animals_in_zoo)
 
+
+
+songs = ["Like a Rolling Stone", "Satisfaction", "Imagine", "What's Going On", "Respect", "Good Vibrations"]
+playcounts = [78, 29, 44, 21, 89, 5]
+
+plays = {key: value for key, value in zip(songs, playcounts)}
+
+
+
