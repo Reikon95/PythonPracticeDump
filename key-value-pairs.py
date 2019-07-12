d48b698512ac6d19d55bfc22bcecb8c899d292ef
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-12 14:28:46 +0100

    Create key-value-pairs.py

---
+++ key-value-pairs.py
@@ -0,0 +1,13 @@
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
+
+
+'''
+simple structure, they go in pairs. First one is key, second one value.
+
+
+Easy
+
+'''
