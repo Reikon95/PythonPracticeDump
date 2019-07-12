1ff66e1083b34ec50d540f85dbe16621ea26078b
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-12 16:08:52 +0100

    Update key-value-pairs.py

--- key-value-pairs.py
+++ key-value-pairs.py
@@ -110,3 +110,18 @@ lessons = num_exercises.keys()
 
 print(lessons)
 
+
+'''
+Get all values
+
+'''
+
+num_exercises = {"functions": 10, "syntax": 13, "control flow": 15, "loops": 22, "lists": 19, "classes": 18, "dictionaries": 18}
+
+total_exercises = 0
+
+for val in num_exercises.values():
+  total_exercises += val
+  
+print(total_exercises)
+
