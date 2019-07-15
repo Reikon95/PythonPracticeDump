6e38885a5e2f181f2f323e44ae32ee4bcc4c0797
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-15 12:58:29 +0100

    Update files.py

--- files.py
+++ files.py
@@ -18,3 +18,14 @@ Only reading one line of doc
 with open('just_the_first.txt') as first_line_doc:
   first_line = first_line_doc.readline()
   print(first_line)
+
+
+'''
+
+Writing to a document (w means write in this case, after the file name)
+This example opens a document filled with bad bands in writeable mode
+Then we just add BrokeNCYDE because they sucked.
+'''
+
+with open('bad_bands.txt', 'w') as bad_bands_doc:
+  bad_bands_doc.write('BrokeNCYDE')
