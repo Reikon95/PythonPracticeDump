cd402e109d1af4c2ece5b6acf52b9c17a45b35dc
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-15 12:54:49 +0100

    Update files.py

--- files.py
+++ files.py
@@ -7,3 +7,14 @@ Iterating through lines - prints each line ib a doc
 with open('how_many_lines.txt') as lines_doc:
   for line in lines_doc.readlines():
     print(line)
+
+'''
+
+Only reading one line of doc
+
+
+'''
+
+with open('just_the_first.txt') as first_line_doc:
+  first_line = first_line_doc.readline()
+  print(first_line)
