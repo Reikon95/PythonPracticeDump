8788ffc75aaa6138299be792950de4f2fb1a66ee
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-16 15:22:32 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -4,3 +4,11 @@ Classes
 
 class ExampleClass:
   pass
+
+'''
+Methods are functions within classes
+These are then callable 
+'''
+class Rules:
+  def washing_brushes(self):
+    return "Point bristles towards the basin while washing your brushes."
