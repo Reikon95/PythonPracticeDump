75c355e92fe6d8d880c396b0c369ab1820134925
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-16 15:27:00 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -12,3 +12,17 @@ These are then callable
 class Rules:
   def washing_brushes(self):
     return "Point bristles towards the basin while washing your brushes."
+
+'''
+Methods with arguments
+'''
+class Circle:
+  pi = 3.14
+  
+  def area(self, radius):
+    return Circle.pi * radius ** 2
+  
+circle = Circle()
+pizza_area = circle.area(12 / 2)
+teaching_table_area = circle.area(36 / 2)
+round_room_area = circle.area(11460 / 2)
