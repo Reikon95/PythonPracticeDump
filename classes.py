6b6caf5ba6495c0f6275fd877fabf08bddcc8c45
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-17 19:14:36 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -100,3 +100,24 @@ class RecyclingBin(Bin):
   pass
 
 
+'''
+Exception class
+This will raise an exception (an error), if the condition is met. See below
+In this exampke it is raised if the stock is below 1 meaning they are out of stock
+'''
+class OutOfStock(Exception):
+  pass
+
+class CandleShop:
+  name = "Here's a Hot Tip: Buy Drip Candles"
+  def __init__(self, stock):
+    self.stock = stock
+    
+  def buy(self, color):
+    if self.stock[color] < 1:
+      raise OutOfStock
+    self.stock[color] = self.stock[color] - 1
+
+candle_shop = CandleShop({'blue': 6, 'red': 2, 'green': 0})
+candle_shop.buy('blue')
+
