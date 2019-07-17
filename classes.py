b52b37af4af8d317156ebb2c5d12abe052f2a220
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-17 09:17:18 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -61,3 +61,23 @@ how_many_s = [{'s': False}, "sassafrass", 18, ["a", "c", "s", "d", "s"]]
 for element in how_many_s:
   if hasattr(element, "count"):
     print(element.count("s"))
+'''
+Constructing additional methods
+'''
+class Circle:
+  pi = 3.14
+  def __init__(self, diameter):
+    print("Creating circle with diameter {d}".format(d=diameter))
+    # Add assignment for self.radius here:
+    self.radius = (diameter / 2)
+  def circumference(self):
+    circumference = 2 * self.pi * self.radius
+    return circumference
+
+medium_pizza = Circle(12)
+teaching_table = Circle(36)
+round_room = Circle(11460)
+
+print(medium_pizza.circumference())
+print(teaching_table.circumference())
+print(round_room.circumference())
