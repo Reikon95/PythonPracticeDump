a9649b6c6f7d9a79371e5a042a0eeac918edb115
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-18 12:45:28 +0100

    Update classes.py

--- classes.py
+++ classes.py
@@ -142,3 +142,17 @@ class User:
 class Admin(User):
   def edit_message(self, message, new_text):
     message.text = new_text
+'''
+Super
+Addes additional functionality to an existing method 
+'''
+class PotatoSalad:
+  def __init__(self, potatoes, celery, onions):
+    self.potatoes = potatoes
+    self.celery = celery
+    self.onions = onions
+    
+class SpecialPotatoSalad(PotatoSalad):
+  def __init__(self, potatoes, celery, onions):
+    super().__init__(potatoes, celery, onions)
+    self.raisins = 40
