3ebe8cdf26dbe64102048c056d35ce2cf2b068f0
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-26 16:03:13 +0100

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -6,3 +6,11 @@ test_1 = np.array([92, 94, 88, 91, 87])
 test_2 = np.genfromtxt('test_2.csv', delimiter=',')
 
 print(test_2)
+
+test_3_fixed = []
+for i in range(len(test_3)):
+  test_3_fixed.append(test_3[i] + 2)
+
+
+print(test_3)
+print(test_3_fixed)
