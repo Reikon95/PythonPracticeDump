a743d14b6a373cf6ec3179a5c90be73ed1cd7ec2
Cameron Blackwood <38852603+Reikon95@users.noreply.github.com>
2019-07-26 15:57:53 +0100

    Create numpy.py

---
+++ numpy.py
@@ -0,0 +1,8 @@
+import numpy as np
+import csv
+# with open('books.csv') as books_csv:
+test_1 = np.array([92, 94, 88, 91, 87])
+
+test_2 = np.genfromtxt('test_2.csv', delimiter=',')
+
+print(test_2)
